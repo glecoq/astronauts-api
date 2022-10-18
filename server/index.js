4b652b0e7350799779540ac9cc75247e584edbbf
guillaume l <guillaume.lecoq.oc@gmail.com>
2022-10-18 14:28:26 +0200

    quick fix on avoid error connection with app and database at coldstart

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const Router = require("./router");
 const initDb = require("./init_db");
 const http = require("http");
+const { exit } = require("process");
 
-initDb().then(() => {
-    const server = http.createServer(Router.run);
-    server.listen(8080);
-}).catch((err) => console.log(err));
+function initApp(nbRetry) {
+    console.log("nbRetry=", nbRetry);
+    if (nbRetry < 0) {
+        console.error("Can't be connected to the database after retry");
+        exit(1);
+    }
+
+    initDb().then(() => {
+        const server = http.createServer(Router.run);
+        server.listen(8080);
+    }, (_err) => setTimeout(() => initApp(nbRetry - 1), 2000)).catch((err) => console.log(err));
+}
+
+initApp(5);
